14d42d5a066152d5d6d8e9b1fd8ef23fb9594171
hgsgithub <hgutierrezs@gmail.com>
2020-04-02 00:39:01 +0200

    3 copia

--- EjemplosPres/tabla.js
+++ EjemplosPres/tabla.js
@@ -1,9 +1,12 @@
 //Array
-var tabla=[10,20,30,'leon',false,2];
-console.log(tabla);
-console.log(tabla[0]);
+let tabla=[10,20,30,'leon',true,"Fin"];
+console.log("Mi tabla es : "+ tabla);
+console.log("El Primer elemento es: "+ tabla[0]);
+let tamaño=tabla.length;
+console.log("El tamaño de la tabla es " +tamaño);
 // Recorer un Array
 console.log("Recorrer un Array");
-for(var i=0;i=tabla.length;i++) {
-    console.log(tabla[i])
-};
+for (let i=0;i<tamaño;i++) {
+    console.log("Elemento Nº " + i + " es "+ tabla[i]);
+
+}
